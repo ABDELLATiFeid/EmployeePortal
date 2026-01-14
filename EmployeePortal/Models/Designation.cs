99d4049883407f94e7e3a11ff2274e909be6216f
Abdellateef Eid <Abdellateef.Eid@bmw-egypt.com>
2026-01-14 12:09:15 +0200

    Add Models-ViewModels-AppDBContext-SeedData-EmpService-ConnectionString

---
+++ EmployeePortal/Models/Designation.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeePortal.Models
+{
+    public class Designation
+    {
+        // This class represents job titles or positions within departments, such as Software Developer or HR Manager.
+        // Each designation belongs to a specific department and can be activated or deactivated.
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; } = null!;
+        public bool IsActive { get; set; }
+    }
+}
---
+++ EmployeePortal/Models/EmployeeType.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeePortal.Models
+{
+    public class EmployeeType
+    {
+        // This class represents various types of employment, including Permanent, Temporary, Contract, and Intern.
+        // It helps categorize employees based on their employment status and tracks whether each type is active.
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public List<Employee>? Employees { get; set; }
+    }
+}
